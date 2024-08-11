6ebbdc5f244f4a0c75c8d55fc5e6a347c77fefcd
Vaishnavi Rakhonde <vaishnavirakhonde@gmail.com>
2024-08-11 08:37:36 -0700

    displying platform icons

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Platform } from "../hooks/useGames";
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { IconType } from "react-icons";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+  // index signature
+  const iconMap: { [key: string]: IconType } = {
+    // name: PlayStation
+    // slug: playstation -> better to rely on
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+    android: FaAndroid,
+  };
+  return (
+    <HStack marginY={1}>
+      {" "}
+      // theme.space value
+      {platforms.map((platform) => (
+        <Icon as={iconMap[platform.slug]} color={"gray.600"} />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
