05c3ab8498937cedcc79191854f8b16e0ff033d8
Vaishnavi Rakhonde <vaishnavirakhonde@gmail.com>
2024-08-11 11:09:54 -0700

    displayed genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,7 +14,7 @@ const GameGrid = () => {
       <SimpleGrid
         columns={{ sm: 1, m: 2, l: 3, xl: 5 }}
         padding={10}
-        spacing={10}
+        spacing={3}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
