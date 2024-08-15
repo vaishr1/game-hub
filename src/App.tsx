c321b5bcd0cbb76af419b8db58e49648982a1606
Vaishnavi Rakhonde <vaishnavirakhonde@gmail.com>
2024-08-15 10:00:31 -0700

    enabling speed insights for vercel

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/useGames";
 import SortSelector from "./components/SortSelector";
 import GameHeading from "./components/GameHeading";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 export interface GameQuery {
   genre: Genre | null;
