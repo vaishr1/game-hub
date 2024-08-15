204e85799b4c4e3f348a137e927eedc78568c5c7
Vaishnavi Rakhonde <vaishnavirakhonde@gmail.com>
2024-08-15 00:58:03 -0700

    customize the theme - todo

--- src/theme.ts
+++ src/theme.ts
@@ -4,6 +4,14 @@ const config: ThemeConfig = {
   initialColorMode: "dark",
 };
 
-const theme = extendTheme({ config });
+const theme = extendTheme({
+  config,
+  colors: {
+    gray: {
+      50: "#f9f9f9",
+      300: "#b3b3b3",
+    },
+  },
+});
 
 export default theme;
